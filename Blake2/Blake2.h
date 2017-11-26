71b01527bc519af1a772e0e61748f9ef8103f4fd
Brandon Wiley <brandon@blanu.net>
2017-11-25 22:14:58 -0600

    Initial Commit

---
+++ Blake2/Blake2.h
@@ -0,0 +1,19 @@
+//
+//  Blake2.h
+//  Blake2
+//
+//  Created by Brandon Wiley on 11/25/17.
+//  Copyright © 2017 Operator Foundation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Blake2.
+FOUNDATION_EXPORT double Blake2VersionNumber;
+
+//! Project version string for Blake2.
+FOUNDATION_EXPORT const unsigned char Blake2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Blake2/PublicHeader.h>
+
+
---
+++ Blake2Tests/Blake2Tests.m
@@ -0,0 +1,39 @@
+//
+//  Blake2Tests.m
+//  Blake2Tests
+//
+//  Created by Brandon Wiley on 11/25/17.
+//  Copyright © 2017 Operator Foundation. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Blake2Tests : XCTestCase
+
+@end
+
+@implementation Blake2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
